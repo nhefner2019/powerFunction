be20d6b2d815600be54af067c484f2b6b4ccd018
Nate Hefner <natehefner@Nathans-Air.fios-router.home>
2016-03-09 19:32:09 -0500

    prints base and exponent

---
+++ powerFunction.py
@@ -0,0 +1,10 @@
+def power(base, exponent):
+	print(base)
+	print(exponent)
+	
+	
+#Main Program
+base = raw_input("What will be your base? ")
+exponent = raw_input("What will be your exponent? ")
+
+power(base, exponent)
