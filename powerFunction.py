3818935006b5281ff596b33d74d9c68f57737113
Nate Hefner <natehefner@Nathans-Air.fios-router.home>
2016-03-09 19:34:34 -0500

    prints answer with corresponding base and exponent

--- powerFunction.py
+++ powerFunction.py
@@ -1,10 +1,17 @@
 def power(base, exponent):
-	print(base)
-	print(exponent)
+	
+	tally = 0
+	answer = 1
+	while tally < exponent:
+		answer = answer * base
+		tally += 1
+		
+	print(answer)
+		
 	
 	
 #Main Program
-base = raw_input("What will be your base? ")
-exponent = raw_input("What will be your exponent? ")
+base = int(raw_input("What will be your base? "))
+exponent = int(raw_input("What will be your exponent? "))
 
 power(base, exponent)
