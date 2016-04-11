eaf74d232d2b1cdc3ad803b5ab9e569793d3ca45
Nate Hefner <natehefner@Nathans-Air.fios-router.home>
2016-04-11 19:06:20 -0400

    made the power function recursive

--- powerFunction.py
+++ powerFunction.py
@@ -1,20 +1,30 @@
 #---------------Function----------------------
 def power(base, exponent):
 	
-	tally = 0
-	answer = 1
-	while tally < exponent:
-		answer = answer * base
-		tally += 1
-		
-	return answer
-		
+	if exponent == 0:
+		return 1
 	
+	result = base * power(base, exponent - 1)
+		
+	return result
 	
 #---------------Main Program------------------------
-base = int(raw_input("What will be your base? "))
-exponent = int(raw_input("What will be your exponent? "))
+def int_input(question):
+	
+	answer = raw_input(question)
+	
+	try:
+		#terminating case
+		answer = int(answer)
+		return answer
+	except ValueError:
+		#recursive call
+		return int_input("That is not an integer; try again. ")
+		
+		
+base = int_input("What will be your base? ")
+exponent = int_input("What will be your exponent? ")
 
-answer = power(base, exponent)
+product = power(base, exponent)
 
-print("Your base is {}. Your exponent is {}. The product is {}".format(base, exponent, answer))
+print("Your base is {}. Your exponent is {}. The product is {}".format(base, exponent, product))
