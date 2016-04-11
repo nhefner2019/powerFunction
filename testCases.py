aa970e5be18afe49e0ad404822e5a6007eae2474
Nate Hefner <natehefner@Nathans-Air.fios-router.home>
2016-04-11 19:05:26 -0400

    added test cases

---
+++ testCases.py
@@ -0,0 +1,7 @@
+import powerFunction
+
+#Test cases
+print("4 to the 2nd power is {}.".format(powerFunction.power(4,2)))
+print("2 to the 3rd power is {}.".format(powerFunction.power(2,3)))
+print("7 to the 1st power is {}.".format(powerFunction.power(7,1)))
+print("10 to the 0th power is {}.".format(powerFunction.power(10,0)))
